73762b5ebaa5f60b52c11f7b48898b1d967b8a95
Henrique Barreto <henriquelb28@gmail.com>
2021-05-11 10:03:18 -0300

    feito alguma mudanças de sistema

--- vetores/ACHEI/Program.cs
+++ vetores/ACHEI/Program.cs
@@ -21,43 +21,43 @@ namespace vetores
             Console.ForegroundColor = ConsoleColor.Green;
             if (nome_b == nomes[0])
             {
-                Console.WriteLine("ACHEI PORRA!!! Este nome  está na lista :)");
+                Console.WriteLine("ACHEI !!! Este nome  está na lista :)");
             }
             else if (nome_b == nomes[1])
             {
-                Console.WriteLine("ACHEI PORRA!!! Este nome  está na lista :)");
+                Console.WriteLine("ACHEI !!! Este nome  está na lista :)");
             }
             else if (nome_b == nomes[2])
             {
-                Console.WriteLine("ACHEI PORRA!!! Este nome  está na lista :)");
+                Console.WriteLine("ACHEI !!! Este nome  está na lista :)");
             }
             else if (nome_b == nomes[3])
             {
-                Console.WriteLine("ACHEI PORRA!!! Este nome  está na lista :)");
+                Console.WriteLine("ACHEI !!! Este nome  está na lista :)");
             }
             else if (nome_b == nomes[4])
             {
-                Console.WriteLine("ACHEI PORRA!!! Este nome  está na lista :)");
+                Console.WriteLine("ACHEI !!! Este nome  está na lista :)");
             }
             else if (nome_b == nomes[5])
             {
-                Console.WriteLine("ACHEI PORRA!!! Este nome  está na lista :)");
+                Console.WriteLine("ACHEI !!! Este nome  está na lista :)");
             }
             else if (nome_b == nomes[6])
             {
-                Console.WriteLine("ACHEI PORRA!!! Este nome  está na lista :)");
+                Console.WriteLine("ACHEI !!! Este nome  está na lista :)");
             }
             else if (nome_b == nomes[7])
             {
-                Console.WriteLine("ACHEI PORRA!!! Este nome  está na lista :)");
+                Console.WriteLine("ACHEI !!! Este nome  está na lista :)");
             }
             else if (nome_b == nomes[8])
             {
-                Console.WriteLine("ACHEI PORRA!!! Este nome  está na lista :)");
+                Console.WriteLine("ACHEI !!! Este nome  está na lista :)");
             }
             else if (nome_b == nomes[9])
             {
-                Console.WriteLine("ACHEI PORRA!!! Este nome  está na lista :)");
+                Console.WriteLine("ACHEI !!! Este nome  está na lista :)");
             }
             else
             {
